aae76aabb7becf24dd1d3ef89f0af24cedd29fea
mihir-2005-code <mihirk8933@gmail.com>
2026-06-01 11:24:39 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (85.99%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -1,15 +1,31 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
+        // int n = nums.size();
+        // int max_product = INT_MIN;
+        // for (int i = 0; i < n; i++) {
+        //     int pro = 1;
+        //     for (int j = i; j < n; j++) {
+        //         pro *= nums[j];
+        //         max_product = max(max_product,pro);
+        //     }
+        // }
+        // return max_product;
+        // time complexity - n^2
         int n = nums.size();
-        int max_product = INT_MIN;
+        int ans = INT_MIN;
+        int prefix = 1, sufix = 1;
         for (int i = 0; i < n; i++) {
-            int pro = 1;
-            for (int j = i; j < n; j++) {
-                pro *= nums[j];
-                max_product = max(max_product,pro);
+            if (prefix == 0) {
+                prefix = 1;
             }
+            if (sufix == 0) {
+                sufix = 1;
+            }
+            prefix *= nums[i];
+            sufix *= nums[n-i-1];
+            ans = max(ans,max(prefix,sufix));
         }
-        return max_product;
+        return ans;
     }
 };
