70acb19d91665780438b3b0e62cae44a8672bc1e
mihir-2005-code <mihirk8933@gmail.com>
2026-06-05 11:49:16 +0530

    Time: 15 ms (32.56%) | Memory: 35.1 MB (18.40%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
@@ -12,16 +12,9 @@ public:
         }
         return days_need + 1;
     }
-    int findMax(vector<int>& weights) {
-        int maxi = 0;
-        for (int i = 0; i < weights.size(); i++) {
-            maxi = max(maxi,weights[i]);
-        }
-        return maxi;
-    }
     int shipWithinDays(vector<int>& weights, int days) {
         int n = weights.size();
-        int left = findMax(weights);
+        int left = *max_element(weights.begin(), weights.end());
         int right = accumulate(weights.begin(), weights.end(), 0);;
         while (left <= right) {
             int mid = left + (right - left)/2;
