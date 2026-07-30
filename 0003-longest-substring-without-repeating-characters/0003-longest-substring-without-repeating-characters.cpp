1e1d9f8d516c4f0b867dddb3cf71ec8b9a266732
Mihir Kumar Behera <mihirk8933@gmail.com>
2026-07-30 19:37:39 +0530

    Time: 4 ms (73.25%), Space: 19.2 MB (6.93%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -4,16 +4,16 @@ public:
         vector<int> hash(256,-1);
         int l = 0;
         int r = 0;
-        int maxlen = 0;
+        int maxLen = 0;
         while (r < s.size()) {
             if (hash[s[r]] != -1 && hash[s[r]] >= l) {
                 l = hash[s[r]] + 1;
-            }
+            } 
             int len = r - l + 1;
-            maxlen = max(len,maxlen);
+            maxLen = max(maxLen,len);
             hash[s[r]] = r;
             r++;
         }
-        return maxlen;
+        return maxLen;
     }
 };
